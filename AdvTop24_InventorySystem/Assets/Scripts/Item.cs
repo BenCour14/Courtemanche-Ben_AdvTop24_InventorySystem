0a335046b602a02420bd90c16bc313fd8b2409fc
BenCour14 <bcour4@gmail.com>
2024-09-30 12:38:06 -0400

    Creating the Abstract Class for Items
    - Derived item classes must define a Name
    - If not overridden default classes will have a default of 30 in a stack
    - If a UI image is not overridden, then the No Item Icon will appear
    - Learned about Resources.Load

---
+++ AdvTop24_InventorySystem/Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+// Will display the class in the editor
+[System.Serializable]
+public abstract class Item
+{
+    // Abstract tag means this method must be overridden in derived classes 
+    public abstract string GiveName();
+
+    // Virtual tags, if not overridden, will default to its code  
+    public virtual int MaxStacks()
+    {
+        return 30;
+    }
+
+    // Using Resource.Load to retreive the UI sprites from the Resources folder. Because this is a virtual method if nothing is overridden then the No Item Image Icon will appear
+    public virtual Sprite GiveItemImage()
+    {
+        return Resources.Load<Sprite>("UI/Item Images/No Item Image Icon");  
+    }
+
+}
