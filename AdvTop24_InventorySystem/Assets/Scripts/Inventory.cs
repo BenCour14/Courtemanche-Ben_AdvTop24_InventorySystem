48930f078f7263b465d1413e84dd8c68641a7e84
BenCour14 <bcour4@gmail.com>
2024-10-09 17:31:20 -0400

    Refreshing Inventory
    - Method to refresh inventory to see if more panels need to be added based on inventory size
    - Protected inventorySize variable with a getter and setter that caps it at 50
    - Added a script for item panels that sets up the required references

--- AdvTop24_InventorySystem/Assets/Scripts/Inventory.cs
+++ AdvTop24_InventorySystem/Assets/Scripts/Inventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Inventory : MonoBehaviour
 {
@@ -15,9 +16,33 @@ public class Inventory : MonoBehaviour
     public GameObject itemPanel;
     public GameObject itemPanelGrid;
 
+    // Ref to the existing panels in the inventory so it can be updated if more slots are needed
+    private List<ItemPanel> existingPanels = new List<ItemPanel>();
+
     [Space]
     // Variable to set the inventory size
-    public int inventorySize = 24;
+    private int inventorySize = 24;
+
+    // Getter and setter to protect inventory size from being altered
+    public int InventorySize
+    {
+        get { return inventorySize; }
+        set
+        {
+            // Inventory size is capped at 50
+            if(value > 50)
+            {
+                inventorySize = 50;
+                RefreshInventory();
+            }
+            else
+            {
+                inventorySize = value;
+                RefreshInventory();
+            }
+            
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -53,6 +78,53 @@ public class Inventory : MonoBehaviour
                 Cursor.lockState = CursorLockMode.Confined;
             }
         }
-        
+
+    }
+
+    public void RefreshInventory()
+    {
+        // Store all existing panels in a list
+        existingPanels = itemPanelGrid.GetComponentsInChildren<ItemPanel>().ToList();
+
+        // Checking if the existing amount of item panels is less than inventory size to determine if more item panels need to be added
+        if (existingPanels.Count < inventorySize)
+        {
+            // Calculate amount of panels to create
+            int amountToCreate = inventorySize - existingPanels.Count;
+
+            // Loop to instantiate the additional panels
+            for (int i = 0; i < amountToCreate; i++)
+            {
+                GameObject newPanel = Instantiate(itemPanel, itemPanelGrid.transform);
+                existingPanels.Add(newPanel.GetComponent<ItemPanel>());
+            }
+        }
+
+        int index = 0;
+        foreach (ItemSlotInfo i in items)
+        {
+            i.name = "" + (index + 1);
+            if (i.item != null) i.name += ": " + i.item.GiveName();
+            else i.name += ": --";
+
+            ItemPanel panel = existingPanels[index];
+            if (panel != null)
+            {
+                panel.name = i.name + " Panel";
+
+                panel.inventory = this;
+                panel.itemSlot = i;
+                if (i.item != null)
+                {
+                    panel.itemImage.gameObject.SetActive(true);
+                    panel.itemImage.sprite = i.item.GiveItemImage();
+                    panel.stacksText.gameObject.SetActive(true);
+                    panel.stacksText.text = "" + i.stacks;
+                }
+
+            }
+
+        }
+
     }
 }
